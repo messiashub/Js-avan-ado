7f7fe1f0c92bcfb28d6a4754907c0426b8012883
claudinei messias <claudineimessias@hotmail.com>
2020-11-11 00:04:09 -0300

    Arrow Functions

---
+++ Arrow Functions/script.js
@@ -0,0 +1,15 @@
+// Arrow Functions
+
+// Modo normal de criar  funções
+
+function dobro(x){
+    return x * 2;
+}
+console.log(dobro(2));
+
+//arrow function
+
+let triplo = x => x * 3   // forma simplificada caso tenha somente um parâmetro
+    
+
+console.log(triplo(9));
