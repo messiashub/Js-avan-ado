a368ce96b77ad120fcd1d8de5565fcac00801e85
claudinei messias <claudineimessias@hotmail.com>
2020-11-11 12:13:33 -0300

    Callbacks

---
+++ Callback/script.js
@@ -0,0 +1,16 @@
+
+let usuarios = ["Adriano","Nei","Silvia"];
+
+function inserirUsuario(nome,callback){
+    setTimeout(() =>{
+        usuarios.push(nome);
+        callback();
+    },3000);
+
+}
+
+function listarUsuarios(){
+    console.log(usuarios);
+}
+
+inserirUsuario("Igor",listarUsuarios);
